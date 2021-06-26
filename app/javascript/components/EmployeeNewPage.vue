17d0a7fd445e310dd123d807f315120f9a9df062
Makotox0207 <xiaoxishicheng@gmail.com>
2021-06-27 07:02:56 +0900

    templateの修正

--- app/javascript/components/EmployeeNewPage.vue
+++ app/javascript/components/EmployeeNewPage.vue
@@ -1,47 +1,7 @@
 <template>
 <div>
-  <form :errors="errors" :employee="employee" @submit="createEmployee">
-    <!-- Vue コンポーネントの errors データに要素が格納されている場合にそれらをリストアップ -->
-    <div v-if="errors.length != 0">
-      <ul v-for="e in errors" :key="e">
-       <li><font color="red">{{ e }}</font></li>
-      </ul>
-    </div>
-
-    <div>
-      <label >Name</label>
-      <input v-model="employee.name" type="text">
-    </div>
-    <div>
-      <label >Department</label>
-      <input v-model="employee.department" type="text">
-    </div>
-    <div>
-      <label >Gender</label>
-      <select v-model="employee.gender">
-        <option>other</option>
-        <option>male</option>
-        <option>female</option>
-      </select>
-    </div>
-    <div>
-      <label >Birth</label>
-      <input v-model="employee.birth" type="date">
-    </div>
-    <div>
-      <label >Joined date</label>
-      <input v-model="employee.joined_date" type="date">
-    </div>
-    <div>
-      <label >Payment</label>
-      <input v-model="employee.payment" type="number" min=0>
-    </div> 
-    <div>
-      <label >Note</label>
-      <input v-model="employee.note" type="text">
-    </div>
-    <button type="submit">送信</button>
-  </form>
+  <employee-form-pane :errors="errors" :employee="employee" @submit="createEmployee">
+  </employee-form-pane>
 </div>
 </template>
 
